a155bc6338949049e790fc71eff87c0c0513b28a
sinister524 <sinister524@gmail.com>
2018-09-03 20:34:17 +0300

    Подготовил первое задание

--- src/ru/geekbrains/algorithmsJava1/Main.java
+++ src/ru/geekbrains/algorithmsJava1/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        Находим среднее арифмитическое O(n)
+        int [] numbers = {5,65,13,55,88,65,99,146};
+        int k = 0;
+        for (int i = 0; i <numbers.length ; i++) {
+            k+=numbers [i];
+        }
+        int average;
+        average = k / numbers.length;
+        System.out.println("Среднее арифмитическое: " + average);
     }
 }
