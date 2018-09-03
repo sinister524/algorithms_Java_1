be8da7dd7037e345620c018194c92d68323b0c47
sinister524 <sinister524@gmail.com>
2018-09-03 21:09:19 +0300

    Подготовил задание 3

--- src/ru/geekbrains/algorithmsJava1/Main.java
+++ src/ru/geekbrains/algorithmsJava1/Main.java
@@ -21,6 +21,12 @@ public class Main {
         for (int i = 0; i <numbers.length ; i++) {
             System.out.print(numbers [i] + " ");
         }
-        
+        System.out.println(" ");
+//        Поиск минимального элемента массива O (n)
+        int min = numbers [0];
+        for (int i = 0; i <numbers.length  ; i++) {
+            if (min>numbers[i]) {min = numbers [i];}
+        }
+        System.out.print("Минимальный элемент массива: " + min);
     }
 }
